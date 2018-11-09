bba5ca85444c2037f4b06913ec46876c1cfdb87e
Pranavshub <pranavdhinakar@gmail.com>
2018-11-08 19:21:12 -0800

    Add files via upload

--- GambleMachine/static/boolean-variable.js
+++ GambleMachine/static/boolean-variable.js
@@ -16,11 +16,34 @@ var no = document.getElementById('no');
 function firstWin(){
 	win = 1;
 	document.getElementById('statement').src = "../static/images/win1.png"
-	document.getElementById('firstWin').onclick = secondWin();
-
+	document.getElementById('button').onclick = secondWin;
 }
+
 function secondWin(){
 	win = 1;
 	document.getElementById('statement').src = "../static/images/win2.png"
-	document.getElementById('firstWin').onclick = thirdWin();
+	document.getElementById('button').onclick = thirdWin;
+}
+
+function thirdWin(){
+	win = 1;
+	document.getElementById('statement').src = "../static/images/win3.png"
+	document.getElementById('button').onclick = fourthWin;
+}
+
+function fourthWin(){
+	win = 1;
+	document.getElementById('statement').src = "../static/images/win4.png"
+	document.getElementById('button').onclick = fifthWin;
+}
+
+function fifthWin(){
+	win = 1;
+	document.getElementById('statement').src = "../static/images/win5.png"
+	document.getElementById('button').onclick = loss;
+}
+function loss(){
+	win = 1;
+	//document.getElementById('statement').src = "../static/images/win2.png"
+	//document.getElementById('button').onclick = thirdWin;
 }
